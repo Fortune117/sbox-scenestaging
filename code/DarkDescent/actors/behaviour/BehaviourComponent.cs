ebe83e9fc70735a3ad8dc833cfd89a70f2b9e4e7
Fortune <7847372+Fortune117@users.noreply.github.com>
2023-10-28 23:22:40 +1100

    add flameskull, start work on behaviour components

---
+++ code/DarkDescent/actors/behaviour/BehaviourComponent.cs
@@ -0,0 +1,21 @@
+using Sandbox;
+
+namespace DarkDescent.Actor;
+
+/// <summary>
+/// Controls how our lil actor should think, act and feel about life in general.
+/// </summary>
+public class BehaviourComponent : BaseComponent
+{
+	private ActorComponent ActorComponent { get; set; }
+
+	public override void OnStart()
+	{
+		ActorComponent = GetComponent<ActorComponent>();
+
+		if ( ActorComponent is null )
+		{
+			Log.Error( $"No Actor Component on {GameObject.Name} for Behaviour Component!" );
+		}
+	}
+}
