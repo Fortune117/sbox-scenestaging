586593ebae5ed97ce6443750294c1d6d27039e94
Sol Scobie <scobiesolomon@gmail.com>
2023-11-19 01:15:02 +1100

    Skeleton mirroring fixes(?)
    Test it yell at me if it's still fucked

---
+++ code/DarkDescent/actors/Actor.cs
@@ -0,0 +1,17 @@
+﻿using Sandbox;
+
+namespace DarkDescent.Actor;
+
+[Prefab]
+public class ActorEntity : AnimatedEntity
+{
+	public override void Spawn()
+	{
+		base.Spawn();
+		
+		EnableAllCollisions = true;
+		EnableDrawing = true;
+		EnableHitboxes = true;
+		SurroundingBoundsMode = SurroundingBoundsType.Hitboxes;
+	}
+}
---
+++ code/DarkDescent/components/PickupTargetComponent.cs
@@ -0,0 +1,18 @@
+﻿using Sandbox;
+
+namespace DarkDescent.Components;
+
+public partial class PickupTargetComponent : BaseComponent
+{
+	/// <summary>
+	/// How much strength is required to pick up this object.
+	/// </summary>
+	[Property, Range( 0, 100 )]
+	public float StrengthThreshold { get; set; } = 10;
+
+	/// <summary>
+	/// How much strength below the threshold can we be and still move the object?
+	/// </summary>
+	[Property,  Range( 0, 100 )]
+	public float StrengthLeeway { get; set; } = 5;
+}
